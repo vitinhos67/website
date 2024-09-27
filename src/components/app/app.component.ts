9e9696ee02e111a8544d8b77af204d7715e35565
Victor Santos da Silva <santtosvictor42@gmail.com>
2024-09-27 09:34:13 -0300

    first commit

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { MainContentComponent } from "../main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
